b583e34b4366b40497b2acd6b9b854d65adab8a5
xamey <ecallemax@gmail.com>
2022-01-29 17:09:48 +0100

    add to docs

---
+++ docs/index.98dc266d.js
@@ -0,0 +1,15 @@
+// function getDownloadApi(event) {
+//     debugger;
+//     fetch(`https://soundmint-public.s3.amazonaws.com/KLOUD_REVEALED/meta/${id}`).then(resp => {
+//     }).catch(_ => {
+//         new Snackbar({
+//             message: "Impossible to fetch that ID."
+//         });
+//     });
+// }
+document.querySelector("#input").addEventListener("submit", function(e) {
+    debugger;
+    if (!isValid) e.preventDefault(); //stop form from submitting
+});
+
+//# sourceMappingURL=index.98dc266d.js.map
